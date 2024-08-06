db6777c5ffc9ad01c60eadd3f5aab2f6225c87a3
AnasAdel711 <anasadel.tu7@gmail.com>
2024-08-06 18:41:47 +0300

    Add Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import HomePage from "./pages/HomePage"
+import Navbar from "./components/Navbar"
 
 function App() {
 
   return (
    <BrowserRouter>
+   <Navbar />
    <Routes>
     <Route path="/" element={<HomePage />} />
    </Routes>
