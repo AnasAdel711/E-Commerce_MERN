56fde6c2557a52171fda0271a400bbfacd3ed416
AnasAdel711 <anasadel.tu7@gmail.com>
2024-09-04 18:20:47 +0300

    Login Page + Routing

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,14 +13,17 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import AdbIcon from '@mui/icons-material/Adb';
 import { useAuth } from '../context/Auth/AuthContext';
+import { Button, Grid } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function ResponsiveAppBar() {
 
-  const {username, token} = useAuth();
+  const {username, isAuthenticated} = useAuth();
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
+
+  const navigate = useNavigate()
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -37,7 +40,9 @@ function ResponsiveAppBar() {
     setAnchorElUser(null);
   };
 
-  console.log('From navbar', {username, token})
+  const handleLogin = () => {
+    navigate('/login')
+  }
 
   return (
     <AppBar position="static">
@@ -108,10 +113,18 @@ function ResponsiveAppBar() {
             </Typography>
 
             <Box sx={{ flexGrow: 0 }}>
+              {isAuthenticated ? <>
                 <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
+                  <Grid container alignItems={"center"} justifyContent={"center"} gap={2}>
+                  <Grid item>
+                    <Typography>{username}</Typography>
+                  </Grid>
+                  <Grid item>
+                    <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                        <Avatar alt={username || ''} src="/static/images/avatar/2.jpg" />
+                    </IconButton>
+                  </Grid>
+                  </Grid>
                 </Tooltip>
                 <Menu
                 sx={{ mt: '45px' }}
@@ -129,12 +142,14 @@ function ResponsiveAppBar() {
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
                 >
-                {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
+                    <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">My Orders</Typography>
                     </MenuItem>
-                ))}
+                    <MenuItem onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">Logout</Typography>
+                    </MenuItem>
                 </Menu>
+              </> : <Button variant='contained' color='success' onClick={handleLogin}>Login</Button>}
             </Box>
           </Box>
         </Toolbar>
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -4,11 +4,9 @@ import { BASE_URL } from '../constants/baseUrl'
 import { useAuth } from '../context/Auth/AuthContext'
 import { useNavigate } from 'react-router-dom'
 
-const RegisterPage = () => {
+const LoginPage = () => {
 
     const [error, setError] = useState("")
-    const firstNameRef = useRef<HTMLInputElement>(null)
-    const LastNameRef = useRef<HTMLInputElement>(null)
     const emailRef = useRef<HTMLInputElement>(null)
     const passwordRef = useRef<HTMLInputElement>(null)
 
@@ -17,34 +15,30 @@ const RegisterPage = () => {
     const { login } = useAuth();
 
     const onSubmit = async () => {
-        const firstName = firstNameRef.current?.value;
-        const lastName = LastNameRef.current?.value;
         const email = emailRef.current?.value;
         const password = passwordRef.current?.value;
 
         // Validate the form data
-        if(!firstName || !lastName || !email || !password) {
+        if(!email || !password) {
             setError('Check submitted data')
             return;
         }
 
 
         // Make the call to API to create user
-        const response = await fetch(`${BASE_URL}/user/register`, {
+        const response = await fetch(`${BASE_URL}/user/login`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                firstName,
-                lastName,
                 email,
                 password,
             }),
         });
 
         if(!response.ok) {
-            setError("Unable to register user, please try different credentials!")
+            setError("Unable to login user, please try different credentials!")
             return;
         }
 
@@ -56,20 +50,17 @@ const RegisterPage = () => {
         }
 
         login(email, token)
-
-        navigate('/')
+        navigate("/");
     }
 
     return (
         <Container>
             <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", mt: 4}}>
-            <Typography variant='h6'>Register New Account</Typography>
+            <Typography variant='h6'>Login To Your Account</Typography>
             <Box sx={{display: "flex", flexDirection: "column", gap: 2, mt: 2, border: 1,borderRadius: 3, borderColor: "#f5f5f5", p: 2}}>
-                <TextField inputRef={firstNameRef} label="First Name" name="firstName"/>
-                <TextField inputRef={LastNameRef} label="Last Name" name="lastName"/>
                 <TextField inputRef={emailRef} label="Email" name="email"/>
                 <TextField inputRef={passwordRef} type='password' label="Password" name="password"/>
-                <Button onClick={onSubmit} variant='contained'>Register</Button>
+                <Button onClick={onSubmit} variant='contained'>Login</Button>
                 {error && <Typography sx={{color: "red"}}>{error}</Typography>}
             </Box>
             </Box>
@@ -77,4 +68,4 @@ const RegisterPage = () => {
     )
 }
 
-export default RegisterPage
+export default LoginPage;
